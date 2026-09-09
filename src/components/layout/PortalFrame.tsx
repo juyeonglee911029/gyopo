e8f604bb127afb541d75ec09e9101c0024591b38
JU YEONG LEE <151415058+juyeonglee911029@users.noreply.github.com>
2026-09-09 07:50:31 -0400

    Delete src/components/layout/PortalFrame.tsx

--- src/components/layout/PortalFrame.tsx
+++
@@ -1,7 +0,0 @@
-export default function PortalFrame({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="portal-frame relative z-10 flex min-w-0 flex-grow flex-col">
-      {children}
-    </div>
-  );
-}
